e6e844012a0a1c10d850b898032a6cfb44837a85
Humza920 <humza.rehman.779@gmail.com>
2025-07-15 05:12:18 +0500

    MONGODB

---
+++ node-js/Class(MONGODB)/app.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require('mongodb');
+console.log(MongoClient);
+const url = "mongodb+srv://humzarehman779:cPTwWUpRkr7xrjX9@cluster0.fnf1uz4.mongodb.net/"
+const client = new MongoClient(url)
+console.log(client);
+
+let datab = "classOne"
+let collection ="data"
+
+async function main() {
+ await client.connect()
+ console.log('Connected successfully to server');   
+ datab = await client.db(datab)
+ console.log(datab.collection(collection) + "database");
+ 
+ 
+}
+main()
