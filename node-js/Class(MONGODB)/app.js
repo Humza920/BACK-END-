ec98dcac2777e0ecc75cffc950de75cf11dab774
Humza920 <humza.rehman.779@gmail.com>
2025-07-15 06:55:01 +0500

    WORKING

--- node-js/Class(MONGODB)/app.js
+++ node-js/Class(MONGODB)/app.js
@@ -1,18 +1,30 @@
 const { MongoClient } = require('mongodb');
 console.log(MongoClient);
-const url = "mongodb+srv://humzarehman779:<>pass<>@cluster0.fnf1uz4.mongodb.net/"
+const url =  "mongodb+srv://humzarehman779:cPTwWUpRkr7xrjX9@cluster0.fnf1uz4.mongodb.net/"
 const client = new MongoClient(url)
 console.log(client);
 
 let datab = "classOne"
 let collection ="data"
 
+  const data = {
+    name: 'Ali Aftab',
+    role: 'Teacher',
+    insitute: "Smit"
+  }
+
 async function main() {
  await client.connect()
  console.log('Connected successfully to server');   
  datab = await client.db(datab)
- console.log(datab.collection(collection) + "database");
+ collection = datab.collection(collection) 
+
+const insertResult = await collection.insertOne(data);
+console.log('Inserted documents =>', insertResult);
  
  
 }
 main()
+.then(console.log)
+.catch(console.error)
+.finally(()=>client.close())
