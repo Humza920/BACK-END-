94a762cbb79f5d6b414a74ab045684c5b316d379
Humza <153664368+Humza920@users.noreply.github.com>
2026-02-03 13:14:14 +0500

    Fix MongoDB connection string in database.js

--- node-js/Class(MONGOOSE)/Backend/src/config/database.js
+++ node-js/Class(MONGOOSE)/Backend/src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 async function connectionWithDb() {
-    await mongoose.connect("mongodb+srv://humzarehman779:cPTwWUpRkr7xrjX9@cluster0.fnf1uz4.mongodb.net/classOne")
+    await mongoose.connect("mongodb+srv://humzarehman779:cluster0.fnf1uz4.mongodb.net/classOne")
 }
 
 module.exports = {
